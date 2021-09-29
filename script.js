aa311bd0a78db8a6aabc480570e0e2bf7154aa35
Magalie Veloso <70386514+magalieveloso@users.noreply.github.com>
2021-09-29 15:55:00 -0400

    changement erreurs chapitre reveil

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-const lereveil={
-    subtitle: "reveil",
-    text:"votre frère vous appelle",
-    img:"assets/phonecall.jpg"
-    };
-
 const chaptersObj={
-    lereveil: "reveil",
-    text:"votre frère vous appelle",
-    img:"assets/phonecall.jpg",
     lereveil:{
         subtitle: "reveil",
         text:"votre frère vous appelle",
         img:"assets/phonecall.jpg"
         }
+        
 };
