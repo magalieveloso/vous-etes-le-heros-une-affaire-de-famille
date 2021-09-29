a2349d37fe6a64e1b8bc996ff2f5ea18ee88d9e8
Magalie Veloso <70386514+magalieveloso@users.noreply.github.com>
2021-09-29 18:14:30 -0400

    ajout nouveaux chapitres

--- script.js
+++ script.js
@@ -1,8 +1,62 @@
 const chaptersObj={
     lereveil:{
-        subtitle: "reveil",
-        text:"votre frère vous appelle",
-        img:"assets/phonecall.jpg"
+        subtitle: "Le reveil",
+        text:"Vous vous reveillez comme d'habitude prêt pour votre journée.",
+        img:"assets/wakeup.jpg"
         }
-        
+    ,appeltelephonique:{
+        subtitle:"L'appel",
+        text:"Soudainement votre frère vous appelle.",
+        img:"assets/phonecall.jpg"
+    }
+    ,larencontre:{
+        subtitle:"La rencontre",
+        text:"Il vous demande de le rejoindre chez lui. N'oubliez pas d'amener le code pour rentrer à l'immeuble.",
+        img:"assets/meetup.jpg"
+    }
+    ,arretfeujaune:{
+        subtitle:"Un feu jaune à l'horizon",
+        text:"Vous voyez le feu de circulation passer du vert au jaune",
+        img:"assets/yellowlight.jpg"
+    }
+    ,pasdestationnement:{
+        subtitle:"Pas de stationnement",
+        text:"Vous arrivez chez votre frère, mais il n'y a pas de stationnement de disponible.",
+        img:"assets/noparking.jpg"
+    }
+    ,solutionstationnement:{
+        subtitle:"Vous trouvez une place de stationnement",
+        text:"Vous stationnez la voiture sur une rue passante à deux minutes de l'habitation de votre frère.",
+        img:"assets/yesparking.jpg"
+    }
+    ,arrivéedevantimmeuble:{
+        subtitle:"L'arrivée devant l'immeuble d'appartements",
+        text:"Vous arrivez devant l'immeuble où habite votre frère.",
+        img:"assets/apartmentbuilding.jpg"
+    }
+    ,codeimmeuble:{
+        subtitle:"Code pour rentrer",
+        text:"Vous êtes dehors de l'immeuble, avez-vous le code pour rentrer et vous rendre à l'appartement?",
+        img:"assets/code.jpeg"
+    }
+    ,prendreascenseur:{
+        subtitle:"L'ascenseur",
+        text:"Prenez-vous l'ascenseur pour vous rendre au bon étage?",
+        img:"assets/elevator.jpg"
+    }
+    ,mauvaisetage:{
+        subtitle:"Mauvais étage",
+        text:"Quand vous prenez l'ascenseur vous arrivez au mauvais étage",
+        img:"assets/spookyhallway.jpg"
+    }
+    ,reprendreascenceur:{
+        subtitle:"Reprendre l'ascenseur",
+        text:"Pour arrivez au bon étage ainsi que l'apartement de votre frère, prenez-vous de nouveau l'ascenseur?",
+        img:"assets/elevator.jpg"
+    }
+    ,arrivéedansappartement:{
+        subtitle:"L'arrivée à l'apartement",
+        text:"Vous arrivez à l'appartement de votre frère sain et sauf malgré tout les détours! Victoire!!",
+        img:"assets/apartment.jpg"
+    }
 };
