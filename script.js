b78ee00f02fe8cff0d65ab73a5ab562861079283
Magalie Veloso <70386514+magalieveloso@users.noreply.github.com>
2021-09-29 14:40:21 -0400

    ajout image appel

---
+++ script.js
@@ -0,0 +1,12 @@
+const lereveil={
+    subtitle: "reveil",
+    text:"votre frère vous appelle",
+    img: function(){
+   console.log("assets/phonecall");
+    }
+
+};
+
+const chaptersObj={
+   lereveil: 
+};
