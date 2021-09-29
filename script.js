7945cb736e95216d647ee761dda8b52662beae60
Magalie Veloso <70386514+magalieveloso@users.noreply.github.com>
2021-09-29 15:23:12 -0400

    changement chaptersobj 2

--- script.js
+++ script.js
@@ -7,5 +7,10 @@ const lereveil={
 const chaptersObj={
     lereveil: "reveil",
     text:"votre frère vous appelle",
-    img:"assets/phonecall.jpg"
+    img:"assets/phonecall.jpg",
+    lereveil:{
+        subtitle: "reveil",
+        text:"votre frère vous appelle",
+        img:"assets/phonecall.jpg"
+        }
 };
